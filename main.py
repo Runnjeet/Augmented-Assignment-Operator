fe05eae31caf5f12dea5bc9cc49c31b597de07b7
Runnjeet <runnjeetshergill@gmail.com>
2021-01-24 23:39:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Augmented Assignment Operator
+
+some_value = 5 
+#(i) some_value = 5 + 2
+#(ii) some_value = some_value + 2
+#(iii) some_value += 2
+# All three would give same answer but we would use (iii) option, as it has less typing and it is next
+some_value += 2
+print(some_value)
